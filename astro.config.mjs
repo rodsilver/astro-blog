82e6ec16e4fd89a587e087a89030fea11450cdf9
Rodrigo Soria <rodsilver83@hotmail.com>
2023-09-20 15:58:57 -0400

    add github page config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://rodsilver.github.io',
+  base: '/astro-blog',
+  integrations: [mdx(), sitemap()],
 });
